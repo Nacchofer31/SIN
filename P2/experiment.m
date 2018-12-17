a691922831e9f586b0931d68d7ac5702e5177d4e
Nacchofer31 <ignaciofer31@gmail.com>
2018-12-17 23:28:55 +0100

    Add resultados and comments in experiment

--- P2/experiment.m
+++ P2/experiment.m
@@ -5,23 +5,23 @@ if (nargin!=3)
   exit(1);
 end;
 
-arg_list = argv(); # Get arguments 
-data = arg_list{1}; # Get de firts argument (data)
-as = str2num(arg_list{2}); # Get de second argument (a)
-bs = str2num(arg_list{3}); # Get de third argument (b)
+arg_list = argv();
+data = arg_list{1};
+as = str2num(arg_list{2});
+bs = str2num(arg_list{3});
 
+#Se cargan las matriz de cada archivo.
 load(data); # Load data
 
-[ N, L ] = size(data); # Load size of matrix data
+[ N, L ] = size(data);
 
-D = L-1; # Load the number of characteristics
+D = L-1;
 
-
+#Convierte el número de clases en etiquetas
 ll = unique(data(:,L));
 C = numel(ll);
 
 rand("seed",23);
-
 data = data(randperm(N),:);
 
 NTr = round(.7*N);
@@ -33,19 +33,22 @@ te = data(NTr+1:N,:);
 printf("       a        b   E   k Ete  Ete (%%)    Ite (%%)\n");
 printf("-------- -------- --- --- --- -------- ----------\n");
 
-for a=as # a=[.1 1 10 100 1000 10000 100000]
+#Entra en el perceptron y lo clasifica
+for a=as
 
-  for b=bs # b=[.1 1 10 100 1000 10000 100000]
+  for b=bs
     
     [w, E, k] = perceptron(data(1:NTr,:), b, a);
-
+    
+    #Se almacena en la clase que pertenece
     rl = zeros(M, 1);
 
     for n=1:M
-
+      #Clase a la que pertenece
       rl(n) = ll(linmach(w,[1 te(n, 1:D)]')); #'
     end
     
+    #Compara las etiquetas
     [nerr m] = confus(te(:, L), rl);
 
     perr = nerr/M;
@@ -67,10 +70,10 @@ for a=as # a=[.1 1 10 100 1000 10000 100000]
 end
 
 printf("\n");
-printf("a: Alpha utilizada\n");
-printf("b: Beta utilizada\n");
-printf("E: Numero de errores producidos en última iteración.\n");
-printf("k: Iteraciones necesarias\n");
-printf("Ete: Error estimado\n");
-printf("Ete (%%): Porcentaje de error\n");
-printf("Ite (%%): Intervalo de confianza\n");
+printf("a -> Alpha\n");
+printf("b -> Beta\n");
+printf("E -> Nº Errores en la última iteración.\n");
+printf("k -> Iteraciones necesarias\n");
+printf("Ete -> Error estimado\n");
+printf("Ete (%%) -> Porcentaje de error\n");
+printf("Ite (%%) -> Intervalo de confianza\n");
